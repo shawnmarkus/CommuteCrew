0d8bff7f2328e325795de7c5c45c4cd465bcbbc7
shawnmarkus <shawnmarkus4200@gmail.com>
2023-05-15 01:23:48 +0530

    added geocoder feature

--- Controller/UserActions.js
+++ Controller/UserActions.js
@@ -3,7 +3,7 @@
 */
 
 const PublishModel = require("../Models/PublishRide");
-
+const NodeGeocoder = require("node-geocoder");
 /*
  * the function getRideList takes :
  * bounds of the circle of source coordinate  in [lng and lat]  format
@@ -42,9 +42,32 @@ const getRideList = async (req, res) => {
     }).populate("providerDetailRef");
 
     if (!rideList.length) {
-      return res.status(404).json({
+      return res.status(200).json({
         status: "NO_RIDE_FOUND",
+        rideList: [],
       });
+    }
+
+    console.log("hurray");
+    const options = {
+      provider: "google",
+      apiKey: process.env.API_KEY, // for Mapquest, OpenCage, Google Premier
+      formatter: null, // 'gpx', 'string', ...
+    };
+
+    const geocoder = NodeGeocoder(options);
+    for (let i = 0; i < rideList.length; i++) {
+      const sourceLocNameRes = await geocoder.reverse({
+        lat: rideList[i].source[1],
+        lon: rideList[i].source[0],
+      });
+      const destinationLocNameRes = await geocoder.reverse({
+        lat: rideList[i].destination[1],
+        lon: rideList[i].destination[0],
+      });
+
+      rideList[i].source = sourceLocNameRes[0].formattedAddress;
+      rideList[i].destination = destinationLocNameRes[0].formattedAddress;
     }
 
     return res.status(200).json({
@@ -53,7 +76,7 @@ const getRideList = async (req, res) => {
     });
   } catch (error) {
     const errorObj = new Error(`Can't fetch for the list from DB: ${error}`);
-    errorObj.statusCode = 500;
+    errorObj.statusCode = 200;
     throw errorObj;
   }
 };
